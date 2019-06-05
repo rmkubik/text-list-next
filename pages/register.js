1cf2c2c5befb37defbffcdd4e84a2caa59db3c0a
Ryan Kubik <rkubik@twilio.com>
2019-06-05 09:24:02 -0700

    Wrap register in layout

--- pages/register.js
+++ pages/register.js
@@ -1,14 +1,18 @@
+import Layout from "../components/Layout";
+
 const RegistrationView = () => {
   return (
-    <form>
-      <label>User Name</label>
-      <input />
-      <label>Password</label>
-      <input />
-      <label>Confirm Password</label>
-      <input />
-      <input type="submit" />
-    </form>
+    <Layout>
+      <form>
+        <label>User Name</label>
+        <input />
+        <label>Password</label>
+        <input />
+        <label>Confirm Password</label>
+        <input />
+        <input type="submit" />
+      </form>
+    </Layout>
   );
 };
 
