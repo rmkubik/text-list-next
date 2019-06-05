eee81802abcb6be8d26245c5f3d54527b303ea0d
Ryan Kubik <rkubik@twilio.com>
2019-06-05 09:25:42 -0700

    Create footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,28 @@
 const Footer = ({ children }) => {
-  return <p>footer</p>;
+  return (
+    <footer className="bg-gray-500">
+      <div className="flex justify-between max-w-4xl mx-auto p-4 md:p-8 text-sm">
+        <p className="text-white">
+          Created by{" "}
+          <a
+            href="https://ryankubik.com"
+            className="font-bold no-underline text-white"
+          >
+            Ryan Kubik
+          </a>
+        </p>
+
+        <p>
+          <a
+            href="https://github.com/rmkubik/text-list"
+            className="font-bold no-underline text-white"
+          >
+            GitHub
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
