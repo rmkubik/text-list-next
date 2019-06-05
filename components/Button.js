7a6d4db7b04ce74bb449c9e904b6807fed0ca879
Ryan Kubik <rkubik@twilio.com>
2019-06-05 09:56:57 -0700

    Add padding and remove comments

--- components/Button.js
+++ components/Button.js
@@ -1,6 +1,6 @@
 const Button = ({ children }) => {
   return (
-    <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+    <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mb-2">
       {children}
     </button>
   );
